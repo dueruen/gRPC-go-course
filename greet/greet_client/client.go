a1cdc8a071dad1e4adb8e083db8adcf875fcfbf2
dueruen <32197042+dueruen@users.noreply.github.com>
2019-09-22 16:53:01 +0900

    ch3 setup

---
+++ greet/greet_client/client.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/dueruen/gRPC-go-course/greet/greetpb"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect %v", err)
+	}
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("Created client %f", c)
+}
---
+++ greet/greet_server/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/dueruen/gRPC-go-course/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func main() {
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
